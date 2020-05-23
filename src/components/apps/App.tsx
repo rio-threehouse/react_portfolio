cd074403ae48870c68ebe15d8a4a7613b5531fcc
Rio Sanka <threehouse.second@gmail.com>
2020-05-23 17:34:07 +0900

    チュートリアル：useState

--- src/components/apps/App.tsx
+++ src/components/apps/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 
 export const App: React.FC = () => {
+  const [count, setCount] = useState(0);
   return(
-    <div>hello world</div>
-  )
+    <div>
+      <p>You clicked { count } times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me!
+      </button>
+    </div>
+  );
 }

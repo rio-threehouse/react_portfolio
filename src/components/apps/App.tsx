ea8bafda5dc496318266512371a33f396475ca45
Rio Sanka <threehouse.second@gmail.com>
2020-05-23 18:57:33 +0900

    チュートリアル：useEffect

--- src/components/apps/App.tsx
+++ src/components/apps/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const App: React.FC = () => {
   const [count, setCount] = useState(0);
+  const [changeTitle, setChangeTitle] = useState(false);
+
+  // ChangeTitleボタンを押した時だけタイトル変更する
+  useEffect(() => {
+    document.title = `You clicked ${count} times` ;
+  }, [changeTitle]);
+
   return(
     <div>
       <p>You clicked { count } times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me!
+      </button>
+      <button onClick={() => setChangeTitle(!changeTitle)}>
+        Change Title!
       </button>
     </div>
   );

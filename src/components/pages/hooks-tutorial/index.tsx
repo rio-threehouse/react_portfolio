7709786040be3befad8a4361b9749f0892b31f02
Rio Sanka <threehouse.second@gmail.com>
2020-05-26 12:14:41 +0900

    eventhandleに渡すアロー関数の引数ありなしの違い

--- src/components/pages/hooks-tutorial/index.tsx
+++ src/components/pages/hooks-tutorial/index.tsx
@@ -6,6 +6,25 @@ export const HooksTutorial: React.FC = () => {
   const history = useHistory()
   const [count, setCount] = useState(0);
   const [changeTitle, setChangeTitle] = useState(false);
+  const [text, setText] = useState('');
+
+  const alertHello = () => alert('hello!')
+  // 配列の型定義…変数名: 型タイプ[]
+  const buttonTexts: {text: string}[] = [
+    {
+      text: "おはようございます"
+    },
+    {
+      text: "こんにちは"
+    },
+    {
+      text: "こんばんは"
+    }
+  ]
+
+  const changeText = (buttonText: string) => {
+    setText(buttonText)
+  }
 
   // ChangeTitleボタンを押した時だけタイトル変更する
   useEffect(() => {
@@ -13,17 +32,37 @@ export const HooksTutorial: React.FC = () => {
   }, [changeTitle]);
 
   return(
-    <div>
-      <p>You clicked { count } times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me!
-      </button>
-      <button onClick={() => setChangeTitle(!changeTitle)}>
-        Change Title!
-      </button>
-      <button onClick={() => history.goBack()}>
-        Go back
-      </button>
-    </div>
+    <>
+      <div>
+        <p>You clicked { count } times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me!
+        </button>
+        <button onClick={() => setChangeTitle(!changeTitle)}>
+          Change Title!
+        </button>
+        <button onClick={() => history.goBack()}>
+          Go back
+        </button>
+      </div>
+
+      <div>
+        <p>↓アロー関数の実験</p>
+        {/* 引数なしのアロー関数をeventhandleに渡す */}
+        <button onClick={alertHello}>
+          Helloアラートボタン
+        </button>
+
+        {/* 引数ありのアロー関数をeventhandleに渡す */}
+        <div>{text}</div>
+        {buttonTexts.map((buttonText) => {
+          return(
+            <button onClick={() => changeText(buttonText.text)}>
+              {buttonText.text}
+            </button>
+          )})
+        }
+      </div>
+    </>
   );
 }

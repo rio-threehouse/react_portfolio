278174410fef9965fe8448432d0ed25c62eee805
Rio Sanka <threehouse.second@gmail.com>
2020-06-01 17:25:37 +0900

    fix トップページ名の変更

--- src/components/advanced/router.tsx
+++ src/components/advanced/router.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { BasicLayout } from '../layout/basic';
-import { PageList } from '../pages/page-list';
+import { TopPage } from '../pages/top-page';
 import { HooksTutorial } from '../pages/hooks-tutorial';
 
 export const Router: React.FC = () => {
+
   return(
     <BrowserRouter>
       <BasicLayout>
         <Switch>
-          <Route exact path="/" component={PageList} />
+          <Route exact path="/" component={TopPage} />
           <Route exact path="/hooks-tutorial" component={HooksTutorial} />
         </Switch>
       </BasicLayout>
